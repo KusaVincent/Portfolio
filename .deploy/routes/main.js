95989b43d33b9dcc53cb062eb5b3bc71d2690c24
unknown <vincentkusa170203@daystar.ac.ke>
2020-02-14 22:24:51 +0300

    mustache + routing

---
+++ .deploy/routes/main.js
@@ -0,0 +1,11 @@
+const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
+const router = vertex.router();
+
+router.get("/", (req, res) => {
+  const data = {
+    greeting: "welcome"
+  };
+  res.render("landing", data);
+});
+
+module.exports = router;
