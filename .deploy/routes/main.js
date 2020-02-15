36e8c740ffa9484f075ecdf69fcb9f419c47f9b9
unknown <vincentkusa170203@daystar.ac.ke>
2020-02-15 08:35:27 +0300

    worked on homepage

--- .deploy/routes/main.js
+++
@@ -1,9 +0,0 @@
-const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
-const router = vertex.router();
-
-router.get("/", (req, res) => {
-  const data = req.context;
-  res.render("landing", data);
-});
-
-module.exports = router;
